74f9be7a8b25d368a7eea82ac56f396168921bf9
Brayan Vallejo <madridvallejo21@gmail.com>
2024-11-16 16:28:39 -0500

    se actualiza ciclo

--- javascritp/script.js
+++ javascritp/script.js
@@ -2,53 +2,55 @@
 
 // realizando un buscador para una pagina de noticias deportivas y los usuarios encuentras las ultimas noticias de su deporte favorito //
 
-let Deporte = prompt("Que deporte quieres ver hoy").toLocaleLowerCase();
-
 const arrayDeporte = [
-    "Liga española resultados Real Madrid 4 vs osasuna 0 ",
-
-    "Los Mets barran alos Yankies en la final de la seria mundial",
-
-    "Por primera en la historia de la NBA un jugador comparte cancha con su hijo y ese es el caso de Lebron James y Su hijO Lebron Junior",
-
-    "Casper Ruud dio la primera gran sorpresa de las Finales ATP ante Carlos Alcaraz y Alexander Zverev empezó su camino en Turín como lo que es: un gran candidato. Despachó rápidamente a Andrey Rublev",
+    "Liga española resultados Real Madrid 4 vs Osasuna 0",
+    "Los Mets barren a los Yankees en la final de la Serie Mundial",
+    "Por primera vez en la historia de la NBA un jugador comparte cancha con su hijo y ese es el caso de LeBron James y su hijo LeBron Jr.",
+    "Casper Ruud dio la primera gran sorpresa de las Finales ATP ante Carlos Alcaraz y Alexander Zverev empezó su camino en Turín como lo que es: un gran candidato. Despachó rápidamente a Andrey Rublev"
 ];
 
-function noticias(deporte){
-       
-    switch(Deporte){
-        
+function noticias(deporte) {
+    
+    switch (deporte.toLowerCase()) { 
         case "futbol":
-        alert("bienvenido alas noticias del mundo del Futbol");
-        alert(arrayDeporte[0]);
-        break;
-        
+            alert("Bienvenido a las noticias del mundo del Fútbol");
+            alert(arrayDeporte[0]);
+            break;
         case "beisbol":
-        alert("Bienvenido alas noticias del mundo del Beisbol");
-        alert(arrayDeporte[1]);
-        break;
-
+            alert("Bienvenido a las noticias del mundo del Béisbol");
+            alert(arrayDeporte[1]);
+            break;
         case "baloncesto":
-        alert("Bienvenido alas noticias del mundo del Baloncesto");
-        alert(arrayDeporte[2]);
-        break;
-
+            alert("Bienvenido a las noticias del mundo del Baloncesto");
+            alert(arrayDeporte[2]);
+            break;
         case "tenis":
-        alert("Bienvenido alas noticias del mundo del tenis");
-        alert(arrayDeporte[3]);
-        break;
-
+            alert("Bienvenido a las noticias del mundo del Tenis");
+            alert(arrayDeporte[3]);
+            break;
         default:
-            alert("Gracias por visitar Sportcenter");
+            alert("Ingrese un deporte válido");
+    }
+}
+
+let Deporte;
+
+do {
+    
+    Deporte = prompt("¿Qué deporte quieres ver hoy (fútbol, béisbol, tenis o baloncesto)? Escribe 'salir' para terminar.");
+
+    if (Deporte === "salir") {
+        alert("Gracias por visitar las noticias deportivas. ¡Hasta luego!");
+        break; 
     }
 
-    Deporte = prompt("Te gustaria ver mas noticias de otro deporte").toLocaleLowerCase(); 
-}   
+    noticias(Deporte);
 
-noticias(Deporte);
+} while (Deporte.toLowerCase() !== "salir"); 
 
-while(Deporte == "si"){
-    Deporte = prompt("Que deporte quieres ver hoy").toLocaleLowerCase();   
-}
-noticias(Deporte);
+
+
+
+
+
 
